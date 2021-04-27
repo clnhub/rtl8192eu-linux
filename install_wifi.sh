b80a992900a44ea1ef69c00a78447ccdafd53c8c
clnhub <42144823+clnhub@users.noreply.github.com>
2021-04-28 00:01:26 +0200

    Update install_wifi.sh

--- install_wifi.sh
+++ install_wifi.sh
@@ -3,7 +3,7 @@ dver=1.0
 dusrdir=/usr/src/$dname-$dver
 sudo rmmod 8192eu
 sudo rmmod rtl8xxxu
-sudo dkms remove -m $dname -v $dver --all
+sudo dkms remove -m $dname -v $dver
 sudo rm -r $dusrdir
 sudo mkdir $dusrdir
 sudo cp -ar . $dusrdir
