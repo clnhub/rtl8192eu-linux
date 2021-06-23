ec54adc9f394a5de2c57dea0ef9b6bcdd1295500
clnhub <42144823+clnhub@users.noreply.github.com>
2021-06-23 17:15:40 +0200

    Improve kernel 5.12(+) support

--- os_dep/linux/recv_linux.c
+++ os_dep/linux/recv_linux.c
@@ -356,11 +356,12 @@ static int napi_recv(_adapter *padapter, int budget)
 #ifdef CONFIG_RTW_GRO
 		if (pregistrypriv->en_gro) {
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 12, 0))
-			if (rtw_napi_gro_receive(&padapter->napi, pskb) != GRO_MERGED_FREE)
+			rtw_napi_gro_receive(&padapter->napi, pskb);
+			rx_ok = _TRUE;				
 #else
 			if (rtw_napi_gro_receive(&padapter->napi, pskb) != GRO_DROP)
-#endif
 				rx_ok = _TRUE;
+#endif
 				
 			goto next;
 		}
