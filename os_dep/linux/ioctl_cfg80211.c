948a2acdfaf5eec8de486533121d00388564cc39
clnhub <daveholden2010@gmail.com>
2022-10-13 20:29:10 +0200

    Improve  kernel 5.11(+) support

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -455,7 +455,7 @@ u8 rtw_cfg80211_ch_switch_notify(_adapter *adapter, u8 ch, u8 bw, u8 offset,
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 19, 0))
 	if (started) {
 		cfg80211_ch_switch_started_notify(adapter->pnetdev, &chdef, 0
-	#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 17, 0))
+	#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 11, 0))
 		, false
 	#endif
 		);
