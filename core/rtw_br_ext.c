be573b3d4e2673de67033df522bd38ff75aff14f
clnhub <daveholden2010@gmail.com>
2022-10-13 14:24:52 +0200

    Add kernel 5.15(+) support

--- core/rtw_br_ext.c
+++ core/rtw_br_ext.c
@@ -17,7 +17,9 @@
 #ifdef __KERNEL__
 	#include <linux/if_arp.h>
 	#include <net/ip.h>
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 15, 0))	
 	#include <net/ipx.h>
+#endif	
 	#include <linux/atalk.h>
 	#include <linux/udp.h>
 	#include <linux/if_pppox.h>
@@ -67,6 +69,31 @@
 #define MAGIC_CODE		0x8186
 #define MAGIC_CODE_LEN	2
 #define WAIT_TIME_PPPOE	5	/* waiting time for pppoe server in sec */
+
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0))
+#define IPX_NODE_LEN	6
+struct ipx_address {
+	__be32  net;
+	__u8    node[IPX_NODE_LEN];
+	__be16  sock;
+};
+
+struct ipxhdr {
+	__be16			ipx_checksum __packed;
+#define IPX_NO_CHECKSUM	cpu_to_be16(0xFFFF)
+	__be16			ipx_pktsize __packed;
+	__u8			ipx_tctrl;
+	__u8			ipx_type;
+#define IPX_TYPE_UNKNOWN	0x00
+#define IPX_TYPE_RIP		0x01	/* may also be 0 */
+#define IPX_TYPE_SAP		0x04	/* may also be 0 */
+#define IPX_TYPE_SPX		0x05	/* SPX protocol */
+#define IPX_TYPE_NCP		0x11	/* $lots for docs on this (SPIT) */
+#define IPX_TYPE_PPROP		0x14	/* complicated flood fill brdcast */
+	struct ipx_address	ipx_dest __packed;
+	struct ipx_address	ipx_source __packed;
+};
+#endif
 
 /*-----------------------------------------------------------------
   How database records network address:
