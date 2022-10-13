6758c3b9e2d19193ea0bd6247766137c473e476b
clnhub <daveholden2010@gmail.com>
2022-10-13 14:31:59 +0200

    Add kernel 5.18(+) support

--- os_dep/osdep_service.c
+++ os_dep/osdep_service.c
@@ -2231,7 +2231,9 @@ static int isFileReadable(const char *path, u32 *sz)
 {
 	struct file *fp;
 	int ret = 0;
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 18, 0))
 	mm_segment_t oldfs;
+#endif	
 	char buf;
 
 	fp = filp_open(path, O_RDONLY, 0);
@@ -2275,7 +2277,9 @@ static int isFileReadable(const char *path, u32 *sz)
 static int retriveFromFile(const char *path, u8 *buf, u32 sz)
 {
 	int ret = -1;
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 18, 0))	
 	mm_segment_t oldfs;
+#endif
 	struct file *fp;
 
 	if (path && buf) {
@@ -2317,7 +2321,9 @@ static int retriveFromFile(const char *path, u8 *buf, u32 sz)
 static int storeToFile(const char *path, u8 *buf, u32 sz)
 {
 	int ret = 0;
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 18, 0))	
 	mm_segment_t oldfs;
+#endif	
 	struct file *fp;
 
 	if (path && buf) {
